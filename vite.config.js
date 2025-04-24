3dcc4f6bff143192a60d54b86ed2a9766059c02a
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-04-24 13:44:00 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './portfolio',
+  base: '/pf/', // Change this to your repository name
   publicDir: 'public',
   build: {
     outDir: 'dist',
